3dc5b103258585021c3d893408ea5ca84c3fad33
Julian Dolby <dolby@us.ibm.com>
2022-06-04 17:36:03 -0400

    non-santizer demo

---
+++ data/demo4.js
@@ -0,0 +1,25 @@
+var document = { URL: "whatever",
+  writeln: function Document_prototype_writeln(x) { },
+  write: function Document_prototype_write(x) { } };
+var id = function _id(x) {
+  var id2 = function _id2(x) { return x; };  
+  return id2(x);
+}; 
+function Id() { this.id = id; }
+function SubId() { }; SubId.prototype = new Id();
+function isUseless(x) {
+    return x.startsWith("http:");
+}
+
+if (Math.random.call(null) > 0) {
+    var id1 = new Id();
+    var text = id1.id.call(document, document.URL);
+    document.write(text);
+    if (isUseless(text)) {
+	document.write(text);
+    }
+} else {
+    var id2 = new SubId();
+    var text = id2.id("not a url");
+    document.writeln(text);
+}
